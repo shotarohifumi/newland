fa7986e4d321f26012bae08932b946ab5ca256bc
PUV00014\owner <lotus.life13@gmail.com>
2023-02-03 12:50:31 +0900

    accordiontry

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(function(){
     $("button").on("click",function(){
         $("body,html").animate({scrollTop:0},500);
     })
+    
+    $(".question").on("click",function(){
+        $(this).next().slideToggle();
+        $(this).toggleClass("on");
+    })
 })
