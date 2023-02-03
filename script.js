8e32abc60c2bd512ba122f1fdcbb0081e2767d3c
Toshiya <toshiya1996s@gmail.com>
2023-02-03 19:22:38 +0900

    btn

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-$(function(){
-    $("button").on("click",function(){
-        $("body,html").animate({scrollTop:0},500);
+$(function () {
+    $(window).on("scroll", function () {
+        let btn = $(".btn").offset().top;
+
+        let wh = $(window).height();
+
+        let position = $(window).scrollTop();
+       
+        if (position + wh >= btn) {
+            $(".btn").removeClass("btn");
+        }
     })
-    
-    $(".question").on("click",function(){
+
+    $("button").on("click", function () {
+        $("body,html").animate({ scrollTop: 0 }, 500);
+    })
+
+    $(".question").on("click", function () {
         $(this).next().slideToggle();
         $(this).toggleClass("on");
     })
